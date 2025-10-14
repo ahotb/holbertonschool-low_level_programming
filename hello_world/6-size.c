ec43c7162e9d4213a85f8859ba3287df05cffc86
ahotb <ah.r9625@gmail.com>
2025-10-14 09:17:45 +0000

    aize

---
+++ hello_world/6-size.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+/**
+*main - printf Hello, printf
+*Return: 0 on tru
+*/
+int main(void)
+{
+printf("Size of a char: %c byte(s)\n", sizeof(char));
+printf("Size of an int: %d byte(s)\n", sizeof(int));
+printf("Size of a long: byte(s)\n", sizeof(long int));
+printf("Size of a long long int: byte(s)\n", sizeof(long long int));
+printf("Size of a float: %f byte(s)\n", sizeof(float));
+return (0);
+}
