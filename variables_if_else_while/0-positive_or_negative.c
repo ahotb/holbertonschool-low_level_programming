d0b1dc007c323e8e234beb4068b68b66b574f770
ahotb <ah.r9625@gmail.com>
2025-10-14 11:25:40 +0000

    if

--- variables_if_else_while/0-positive_or_negative.c
+++ variables_if_else_while/0-positive_or_negative.c
@@ -16,11 +16,11 @@ printf("%d is positive \n", n);
 }
 else if (n < 0)
 {
-printf("%d is zero \n", n);
+printf("%d is negative \n", n);
 }
 else
 {
-printf("%d negative \n", n);
+printf("%d is zero \n", n);
 }
 	return (0);
 }
