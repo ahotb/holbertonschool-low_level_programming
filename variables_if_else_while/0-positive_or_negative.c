d7852baad20bc02d015130ae76a0bc3942197f3d
ahotb <ah.r9625@gmail.com>
2025-10-14 11:21:03 +0000

    if

--- variables_if_else_while/0-positive_or_negative.c
+++ variables_if_else_while/0-positive_or_negative.c
@@ -12,11 +12,11 @@ int main(void)
 	n = rand() - RAND_MAX / 2;
 if (n > 0)
 {
-printf("is positive \n");
+printf("%i is positive \n");
 }
 else if (n < 0)
 {
-printf("is zero \n");
+printf("%i is zero \n");
 }
 else
 {
