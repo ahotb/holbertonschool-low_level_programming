5570a22d8874a713e36ead7568b0e8b3ecaa5bb3
ahotb <ah.r9625@gmail.com>
2025-10-15 07:07:03 +0000

    unm

---
+++ variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,16 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+int un;
+for (un = "0"; un <= "10"; un++)
+putchar(un);
+putchar('\n');
+return (0);
+}
