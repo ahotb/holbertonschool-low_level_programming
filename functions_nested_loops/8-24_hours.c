a34762941eb37c979aeaa8de1521661e7a218c33
ahotb <ah.r9625@gmail.com>
2025-10-16 18:11:10 +0000

    a

--- functions_nested_loops/8-24_hours.c
+++ functions_nested_loops/8-24_hours.c
@@ -1,12 +1,13 @@
 #include "main.h"
-/*
-* Main  jack_bauer
+/**
+* jack_bauer - prints every minute of the day
 *
-* @hour
+* Description: Prints time from 00:00 to 23:59
+* Return: void
 */
 void jack_bauer(void)
 {
-    int hour, minute;
+int hour, minute;
 for (hour = 0; hour <= 23; hour++)
 {
 for (minute = 0; minute <= 59; minute++)
