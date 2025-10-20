a6fd77004b25af9555a464b5580ef191093aa005
ahotb <ah.r9625@gmail.com>
2025-10-20 14:32:33 +0000

    \

--- more_functions_nested_loops/7-print_diagonal.c
+++ more_functions_nested_loops/7-print_diagonal.c
@@ -8,14 +8,21 @@
 */
 void print_diagonal(int n)
 {
-int x;
-
+int x, y;
 if (n > 0)
 {
 for (x = 0; x < n; x++)
 {
+for (y = 0; y < x; y++)
+{
+_putchar(' ');
+}
 _putchar('\\');
-}
-}
 _putchar('\n');
+}
+}
+else
+{
+_putchar('\n');
+}
 }
