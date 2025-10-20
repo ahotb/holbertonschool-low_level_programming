8cbc7a1e73576a22eeafa3e9a7503a52bb079815
ahotb <ah.r9625@gmail.com>
2025-10-20 10:43:49 +0000

    0-9

---
+++ more_functions_nested_loops/1-isdigit.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+* _isupper - checks for uppercase character
+* @c: the character to check
+*
+* Return: 1 if c is uppercase, 0 otherwise
+*/
+int _isdigit(int c)
+{
+if (c >= '0' && c <= '9')
+{
+return (1);
+}
+else
+{
+return (0);
+}
+}
