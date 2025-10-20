747c30e1d1f72af8576821c227a8f9dd1f73684b
ahotb <ah.r9625@gmail.com>
2025-10-20 10:53:39 +0000

    a*b

---
+++ more_functions_nested_loops/2-mul.c
@@ -0,0 +1,12 @@
+#include "main.h"
+
+/**
+* _mul - checks for uppercase character
+* @c: the character to check
+*
+* Return: 1 if c is uppercase, 0 otherwise
+*/
+int mul(int a, int b)
+{
+return (a * b);
+}
