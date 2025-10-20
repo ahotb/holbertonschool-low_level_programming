af176f9c6e6f737fd0a145fe3c8c19ada9f7a6f7
ahotb <ah.r9625@gmail.com>
2025-10-20 15:36:02 +0000

    -squares

--- more_functions_nested_loops/8-print_square.c
+++ more_functions_nested_loops/8-print_square.c
@@ -8,14 +8,20 @@
 */
 void print_square(int size)
 {
-int x;
-
+int col, x;
 if (size > 0)
 {
 for (x = 0; x < size; x++)
 {
+for (col = 0; col < size; col++)
+{
 _putchar('#');
 }
-}
 _putchar('\n');
+}
+}
+else
+{
+_putchar('\n');
+}
 }
