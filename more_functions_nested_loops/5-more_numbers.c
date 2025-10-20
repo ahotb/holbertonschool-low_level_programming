90994a17cc1fd18319fd1639a16a67a5626f01e3
ahotb <ah.r9625@gmail.com>
2025-10-20 13:27:23 +0000

    15

---
+++ more_functions_nested_loops/5-more_numbers.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+/**
+* print_most_numbers- checks for uppercase character
+*
+*
+* Return: 1 if c is uppercase, 0 otherwise
+*/
+void more_numbers(void)
+{
+char x;
+int y;
+for (x = '0'; x <='9'; x++)
+{
+for (y = 0; y <=14; y++)
+{
+if (y>9)
+_putchar(y /10 + '0');
+_putchar(y %10+ '0');
+}
+_putchar('\n');
+}
+}
