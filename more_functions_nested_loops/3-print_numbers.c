15d314ed297d94abd9ad64edea235082180d6acc
ahotb <ah.r9625@gmail.com>
2025-10-20 11:39:40 +0000

    um

---
+++ more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+* print_numbers- checks for uppercase character
+*
+*
+* Return: 1 if c is uppercase, 0 otherwise
+*/
+void print_numbers(void)
+{
+char x;
+for (x = '0'; x <= '9'; x++)
+{
+_putchar(x);
+}
+_putchar('\n');
+}
