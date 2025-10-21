4736bcd0e7eaf9d61f4e19794d06403e997f4bff
ahotb <ah.r9625@gmail.com>
2025-10-21 11:59:21 +0000

    d

--- more_functions_nested_loops/10-print_triangle.c
+++ more_functions_nested_loops/10-print_triangle.c
@@ -6,22 +6,26 @@
 * @size: oen back
 * Return: 1 if c is uppercase, 0 otherwise
 */
+
 void print_triangle(int size)
 {
+
 int c, x, r;
 if (size > 0)
 {
 for (x = 0; x < size; x++)
 {
-for (c = 0; c > size - x - 1; c++)
+for (c = 0; c < size - x - 1; c++)
 {
 _putchar(' ');
 }
-for (r = 0; r > x + 1; r++)
+for (r = 0; r < x + 1; r++)
 {
 _putchar('#');
 }
 _putchar('\n');
+}
+}
 else
 {
 _putchar('\n');
