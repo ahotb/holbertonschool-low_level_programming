02fbb9273540cd17867c8f268a4e0877c5f7741d
ahotb <ah.r9625@gmail.com>
2025-10-21 09:33:48 +0000

    buzz

---
+++ more_functions_nested_loops/9-fizz_buzz.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+/**
+* print_line- checks for uppercase character
+*
+* @n: oen back
+* Return: 1 if c is uppercase, 0 otherwise
+*/
+int main(void)
+{
+int x;
+for (x = 1; x <= 100; x++)
+
+if (x % 3 == 0)
+{
+printf("Fizz ");
+}
+else if (x % 5 == 0)
+{
+printf("Buzz ");
+}
+else 
+{
+printf(" %d ",x);
+}
+return (0);
+}
