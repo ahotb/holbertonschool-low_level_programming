924430a350af358574b812f6349d8ba80e40aee8
ahotb <ah.r9625@gmail.com>
2025-10-21 09:53:07 +0000

    buzz

--- more_functions_nested_loops/9-fizz_buzz.c
+++ more_functions_nested_loops/9-fizz_buzz.c
@@ -1,6 +1,6 @@
 #include <stdio.h>
 /**
-* print_line- checks for uppercase character
+* main - prints numbers 1-100 with Fizz, Buzz, FizzBuzz
 *
 * @FizzBuzz: oen back
 * Return: 1 if c is uppercase, 0 otherwise
