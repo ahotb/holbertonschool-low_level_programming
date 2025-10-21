14d4af6cc4ec3f03c69dabb79a44b0a1de3dda8d
ahotb <ah.r9625@gmail.com>
2025-10-21 09:46:47 +0000

    buzz

--- more_functions_nested_loops/9-fizz_buzz.c
+++ more_functions_nested_loops/9-fizz_buzz.c
@@ -2,14 +2,13 @@
 /**
 * print_line- checks for uppercase character
 *
-* @n: oen back
+* @FizzBuzz: oen back
 * Return: 1 if c is uppercase, 0 otherwise
 */
 int main(void)
 {
 int x;
 for (x = 1; x <= 100; x++)
-
 if (x % 15 == 0)
 {
 printf("FizzBuzz ");
@@ -24,7 +23,7 @@ printf("Buzz ");
 }
 else
 {
-printf(" %d ",x);
+printf("%d ",x);
 }
 printf("\n");
 return (0);
