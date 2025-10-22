98ce691d36339d1496c3923f9807eb37845a52e5
ahotb <ah.r9625@gmail.com>
2025-10-22 09:59:16 +0000

    16

---
+++ pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,20 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+* _strlen - one  pointer
+* @s: pointer to an int
+*
+* Return: void
+*/
+int _strlen(char *s)
+{
+int i;
+int c = 0;
+
+for (i = 0; s[i] != '\0'; i++)
+{
+c++;
+}
+return (0);
+}
