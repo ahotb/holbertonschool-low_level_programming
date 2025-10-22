e20793b397826a508dfa4c144b1647c32faf8d47
ahotb <ah.r9625@gmail.com>
2025-10-22 15:31:00 +0000

    59

---
+++ pointers_arrays_strings/7-puts_half.c
@@ -0,0 +1,22 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+*  puts_half - one  pointer
+* @str: pointer to an int
+*
+* Return: void
+*/
+void puts_half(char *str)
+{
+int i;
+int n;
+int x;
+for (n = 0; str[n] != '\0' ;n++)
+x = ( n +1) / 2;
+for (i = x; str[i] != '\0'; i++)
+{
+_putchar(str[i]);
+}
+_putchar('\n');
+}
