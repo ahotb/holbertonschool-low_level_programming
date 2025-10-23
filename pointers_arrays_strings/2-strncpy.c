20ca5c6d5184fa298dd543021b615cb03fee635a
ahotb <ah.r9625@gmail.com>
2025-10-23 20:12:16 +0000

    s

---
+++ pointers_arrays_strings/2-strncpy.c
@@ -0,0 +1,23 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+* *_strncpy - one  pointer
+* @dest: pointer to an int
+* @src : pointer to an int
+* @n: int
+* Return: void
+*/
+char *_strncpy(char *dest, char *src, int n)
+{
+int i;
+for (i = 0; i < n && src[i] != '\0'; i++)
+{
+dest[i] = src[i];
+for (; i < n; i++)
+{
+dest[i] = '\0';
+}
+}
+return (dest);
+}
