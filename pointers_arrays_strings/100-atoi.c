66df38ae0992d3e0a9b61192453eeb54a9e91799
ahotb <ah.r9625@gmail.com>
2025-10-23 10:54:47 +0000

    a

---
+++ pointers_arrays_strings/100-atoi.c
@@ -0,0 +1,30 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+* _atoi - one  pointer
+* @s: pointer to an int
+*
+* Return: void
+*/
+int _atoi(char *s)
+{
+int i;
+int sng = 1;
+int r = 0;
+for (i = s; s[i] != '\0' && !s[i] >= '0' && s[i] <= '9' && s[i] != '+' && s[i] != '-'); i++)
+;
+for (;s[i] == '+' || s[i] == '-'; i++)
+{
+if (s[i] == '-')
+{
+sng = sng * 1;
+}
+}
+for (i = s; s[i] >= '0' && s[i] <= '9'; i++)
+{
+r = r * 10 + (s[i] - '0');
+}
+return (r * sng);
+}
+
