51bdc5f205a652fe0e886eae8dd0a4389614027e
ahotb <ah.r9625@gmail.com>
2025-10-23 20:38:06 +0000

    s

--- pointers_arrays_strings/2-strncpy.c
+++ pointers_arrays_strings/2-strncpy.c
@@ -1,13 +1,13 @@
 #include "main.h"
-#include <stdio.h>
 
 /**
-* *_strncpy - one  pointer
-* @dest: pointer to an int
-* @src : pointer to an int
-* @n: int
-* Return: void
-*/
+ * _strncpy - copies a string
+ * @dest: destination buffer
+ * @src: source string
+ * @n: number of bytes to copy
+ *
+ * Return: pointer to dest
+ */
 char *_strncpy(char *dest, char *src, int n)
 {
 int i;
@@ -17,6 +17,7 @@ dest[i] = src[i];
 }
 for (; i < n; i++)
 {
-dest[i] =Ù'\0';
+dest[i] = '\0';
 }
+return (dest);
 }
