3e9e66a3c5b28325546bb6c2524611b3ae87aece
ahotb <ah.r9625@gmail.com>
2025-10-23 15:51:46 +0000

    a

--- pointers_arrays_strings/0-strcat.c
+++ pointers_arrays_strings/0-strcat.c
@@ -1,22 +1,22 @@
-#include "main.h"
+\#include "main.h"
 #include <stdio.h>
 
 /**
-* print_rev - one  pointer
-* @s: pointer to an int
-*
+* *_strcat - one  pointer
+* @dest: pointer to an int
+* @src : pointer to an int
 * Return: void
 */
 char *_strcat(char *dest, char *src)
 {
 int m;
 int i;
-for (m = 0;dest[m] != '\0'; m++)
+for (m = 0; dest[m] != '\0'; m++)
 ;
-for (i = 0;src[i] != '\0'; i++)
+for (i = 0; src[i] != '\0'; i++)
 {
 dest[m] = src[i];
 m++;
 }
-return dest;
+return (dest);
 }
