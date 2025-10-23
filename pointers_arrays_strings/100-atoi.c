a18a804786b38a83093d82488d09e0067cf5f5f7
ahotb <ah.r9625@gmail.com>
2025-10-23 11:36:40 +0000

    a

--- pointers_arrays_strings/100-atoi.c
+++ pointers_arrays_strings/100-atoi.c
@@ -13,7 +13,6 @@ int i;
 int sign = 1;
 int result = 0;
 int found_digit = 0;
-
 for (i = 0; s[i] != '\0'; i++)
 {
 if (s[i] == '-' && !found_digit)
@@ -26,12 +25,19 @@ else if (s[i] == '+' && !found_digit)
 else if (s[i] >= '0' && s[i] <= '9')
 {
 found_digit = 1;
+if (sign == 1)
+{
 result = result * 10 + (s[i] - '0');
+}
+else
+{
+result = result * 10 - (s[i] - '0');
+}
 }
 else if (found_digit)
 {
 break;
 }
 }
-return (result * sign);
+return (result);
 }
