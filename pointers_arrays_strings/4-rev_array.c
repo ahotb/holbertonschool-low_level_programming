97920cf0da877d91a6da48015b3ba2378c9ba634
ahotb <ah.r9625@gmail.com>
2025-10-24 14:22:05 +0000

    rev

---
+++ pointers_arrays_strings/4-rev_array.c
@@ -0,0 +1,23 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ * reverse_array - copies a string
+ * @a: destination buffer
+ * @n: source string
+ *
+ * Return: pointer to dest
+ */
+void reverse_array(int *a, int n)
+{
+int i;
+int e = n - 1;
+int tmp;
+while (i < e)
+{
+tmp = a[i];
+a[i] = a[e];
+a[e] = tmp;
+i++;
+n--;
+}
+}
