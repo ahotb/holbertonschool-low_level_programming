139ea3e676521b6470de65d1a6b58ab486514843
ahotb <ah.r9625@gmail.com>
2025-10-24 19:57:08 +0000

    1337

--- pointers_arrays_strings/7-leet.c
+++ pointers_arrays_strings/7-leet.c
@@ -10,20 +10,18 @@
 char *leet(char *s)
 {
 int i, j;
-	char lower[] = "aeotl";
-	char upper[] = "AEOTL";
-	char nums[] = "43071";
-
-	for (i = 0; s[i] != '\0'; i++)
-	{
-		for (j = 0; lower[j] != '\0'; j++)
-		{
-			if (s[i] == lower[j] || s[i] == upper[j])
-			{
-				s[i] = nums[j];
-			}
-		}
-	}
-
-	return (s);
+char lower[] = "aeotl";
+char upper[] = "AEOTL";
+char nums[] = "43071";
+for (i = 0; s[i] != '\0'; i++)
+{
+for (j = 0; lower[j] != '\0'; j++)
+{
+if (s[i] == lower[j] || s[i] == upper[j])
+{
+s[i] = nums[j];
+}
+}
+}
+return (s);
 }
