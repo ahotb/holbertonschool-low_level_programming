26421d8b54d7e2f0ef81a923be9e9d5dddcaa299
ahotb <ah.r9625@gmail.com>
2025-10-27 09:56:15 +0000

    cp

---
+++ pointers_arrays_strings/1-memcpy.c
@@ -0,0 +1,19 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+* _memset - one  pointer
+* @dest : pointer to an int
+* @src : pointer to an int
+* @n : pointer to an int
+* Return: void
+*/
+char *_memcpy(char *dest, char *src, unsigned int n)
+{
+unsigned int i;
+for (i = 0; i < n; i++)
+{
+dest[i] = src[i];
+}
+return (dest);
+}
