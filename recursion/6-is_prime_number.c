aa5d6d11730dd0fd0d3de08f09beae81ff11b20e
ahotb <ah.r9625@gmail.com>
2025-10-29 13:14:12 +0000

    a

--- recursion/6-is_prime_number.c
+++ recursion/6-is_prime_number.c
@@ -1,21 +1,25 @@
 #include "main.h"
 
-
 /**
 * spurt_number - To print a text string
 * @n : obshin
-* @x : spurt_number
+* @i : spurt_number
 * Return: 0/1
 */
 
 int spurt_number(int n, int i)
 {
 if (i * i > n)
+{
 return (1);
-if (n % i == 0)
-return (0);
-return spurt_number(n, i + 2);
 }
+else if (n % i == 0)
+{
+return (0);
+}
+return (spurt_number(n, i + 2));
+}
+
 /**
 * is_prime_number - To print a text string
 * @n : obshin
@@ -24,11 +28,16 @@ return spurt_number(n, i + 2);
 int is_prime_number(int n)
 {
 if (n <= 1)
+{
 return (0);
-if (n == 2)
-return (1);
-if (n % 2 == 0)
-return (0);
-return spurt_number(n,  3);
 }
-
+else if (n == 2)
+{
+return (1);
+}
+else if (n % 2 == 0)
+{
+return (0);
+}
+return (spurt_number(n, 3));
+}
