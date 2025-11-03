51249342606ba3c88a76f330f27af5d6566d7a95
ahotb <ah.r9625@gmail.com>
2025-11-03 13:12:43 +0000

    w

--- malloc_free/0-create_array.c
+++ malloc_free/0-create_array.c
@@ -17,16 +17,15 @@ char *arr;
 arr = malloc(size * sizeof(char));
 if (size == 0)
 {
-return NULL;
+return (NULL);
 }
 if (arr == NULL)
 {
-return NULL;
+return (NULL);
 }
 for (i = 0; i < size; i++)
 {
 arr[i] = c;
 }
-
 return (arr);
 }
