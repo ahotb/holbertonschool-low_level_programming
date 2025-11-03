0830cf19e13f6d83bfcf1b2a96d4eb98b6184fe7
ahotb <ah.r9625@gmail.com>
2025-11-03 15:49:17 +0000

    a

---
+++ malloc_free/1-strdup.c
@@ -0,0 +1,31 @@
+#include "main.h"
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * _strdup- prints buffer in hexa
+ * @str: the address of memory to print
+ *
+ * Return: Nothing.
+ */
+
+char *_strdup(char *str)
+{
+int i, cu;
+char *c;
+
+if (str == NULL)
+{
+return (NULL);
+}
+for (cu = 0; str[cu] != '\0'; cu++)
+;
+cu += 1;
+c = malloc(cu * sizeof(char));
+
+for (i = 0; i < cu; i++)
+{
+c[i] = str[i];
+}
+return (c);
+}
