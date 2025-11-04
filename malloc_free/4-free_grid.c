a0124cc76397038ecdb985b6ec64badd0eaea93b
ahotb <ah.r9625@gmail.com>
2025-11-04 14:00:21 +0000

    w

--- malloc_free/4-free_grid.c
+++ malloc_free/4-free_grid.c
@@ -10,34 +10,10 @@
  */
 void free_grid(int **grid, int height)
 {
-int n, i, j, t;
-int **temp;
-if (width <= 0 || height <= 0)
-{
-return (NULL);
-}
-temp = malloc(height * sizeof(int *));
-if (temp == NULL)
-{
-return (NULL);
-}
+int i;
 for (i = 0; i < height; i++)
 {
-temp[i] = malloc(width * sizeof(int));
-if (temp[i] == NULL)
-{
-for (j = 0; j < i; j++)
-free(temp[j]);
-free(temp);
-return (NULL);
+free(grid[i]);
 }
-}
-for (t = 0; t < height; t++)
-{
-for (n = 0; n < width; n++)
-{
-temp[t][n] = (0);
-}
-}
-return (temp);
+free(grid);
 }
