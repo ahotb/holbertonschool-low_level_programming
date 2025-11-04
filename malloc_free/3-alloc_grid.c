59bcbef87e5884a2fb23346c6a41317028c6ce4c
ahotb <ah.r9625@gmail.com>
2025-11-04 12:17:12 +0000

    s

--- malloc_free/3-alloc_grid.c
+++ malloc_free/3-alloc_grid.c
@@ -11,25 +11,27 @@
 
 int **alloc_grid(int width, int height)
 {
-int n, i, t;
+int n, i, j, t;
+int **temp;
 if (width <= 0 || height <= 0)
 {
 return (NULL);
 }
-int **temp = malloc(height * sizeof(int *));
+temp = malloc(height * sizeof(int *));
 if (temp == NULL)
 {
 return (NULL);
 }
 for (i = 0; i < height; i++)
 {
-temp[i] = malloc(width * sizeof(int *));
+temp[i] = malloc(width * sizeof(int));
 if (temp[i] == NULL)
 {
-for (int j = 0; j < i; j++)
-frre(temp[j]);
-frre(temp);
+for (j = 0; j < i; j++)
+free(temp[j]);
+free(temp);
 return (NULL);
+}
 }
 for (t = 0; t < height; t++)
 {
@@ -38,4 +40,5 @@ for (n = 0; n < width; n++)
 temp[t][n] = (0);
 }
 }
+return (temp);
 }
