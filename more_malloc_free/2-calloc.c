714aae8a6a5994b6221e1bcbfe88751a84aef806
ahotb <ah.r9625@gmail.com>
2025-11-05 15:42:59 +0000

    a

---
+++ more_malloc_free/2-calloc.c
@@ -0,0 +1,22 @@
+#include "main.h"
+#include <stdio.h>
+#include <stdlib.h>
+#include <limits.h>
+
+/**
+ * calloc- prints buffer in hexa
+ * @nmemb,: the address of memory to print
+ * @size: the address of memory to print
+ *
+ * Return: Nothing.
+ */
+void *_calloc(unsigned int nmemb, unsigned int size)
+{
+unsigned int i, t, cunt1, cu;
+char *temp;
+
+if (temp == NULL)
+return (NULL);
+}
+return (temp);
+}
