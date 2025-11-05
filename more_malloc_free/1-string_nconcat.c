3035fa1b99e0a068a33a1efa37dc3dfe67127095
ahotb <ah.r9625@gmail.com>
2025-11-05 14:24:41 +0000

    r

--- more_malloc_free/1-string_nconcat.c
+++ more_malloc_free/1-string_nconcat.c
@@ -13,5 +13,21 @@
  */
 char *string_nconcat(char *s1, char *s2, unsigned int n)
 {
-
+int cunt1, cunt2;
+char *temp;
+if (s2 == NULL)
+{
+s2 = "";
+}
+if (s1 == NULL)
+{
+s1 = "";
+}
+for (cunt1 = 0; s1[cunt1] != '\0'; cunt1++)
+;
+for (cunt2 = 0; cunt2 < n; cunt2++)
+;
+temp = malloc(cunt1 + n + 1);
+tmep = cunt1[
+return (temp);
 }
