fc7cd70acdcd2d5353e490654882ca580ba12e64
ahotb <ah.r9625@gmail.com>
2025-11-05 10:54:31 +0000

    r

---
+++ more_malloc_free/1-string_nconcat.c
@@ -0,0 +1,17 @@
+#include "main.h"
+#include <stdio.h>
+#include <stdlib.h>
+#include <limits.h>
+
+/**
+ * string_nconcat- prints buffer in hexa
+ * @s1: the address of memory to print
+ * @s2: the address of memory to print
+ * @n: the address of memory to print
+ *
+ * Return: Nothing.
+ */
+char *string_nconcat(char *s1, char *s2, unsigned int n)
+{
+
+}
