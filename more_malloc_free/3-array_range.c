843e96e9767b03ee9b4db9719b6dab8c3f89dd2e
ahotb <ah.r9625@gmail.com>
2025-11-06 11:50:17 +0000

    malloc

--- more_malloc_free/3-array_range.c
+++ more_malloc_free/3-array_range.c
@@ -14,17 +14,25 @@
 int *array_range(int min, int max)
 {
 unsigned int array;
+int y = 0;
 int *temp;
 if (min > max)
 {
 return (NULL);
 }
-array = min * max
+array = max - min + 1;
 
-temp = malloc(array);
+temp = malloc(array * sizeof(int));
 if (temp == NULL)
 {
 return (NULL);
+}
+
+while ( min != max)
+{
+temp[y] = min;
+y++;
+min++;
 }
 return (temp);
 }
