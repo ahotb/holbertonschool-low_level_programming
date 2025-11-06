6b3cdfe18e638198b3feb3ea0d1a949c61ee239f
ahotb <ah.r9625@gmail.com>
2025-11-06 11:31:52 +0000

    malloc

---
+++ more_malloc_free/3-array_range.c
@@ -0,0 +1,33 @@
+#include "main.h"
+#include <stdio.h>
+#include <stdlib.h>
+#include <limits.h>
+
+/**
+ * string_nconcat- prints buffer in hexa
+ * @s1: the address of memory to print
+ * @s2: the address of memory to print
+ * @n: the address of memory to print
+ *
+ * Return: Nothing.
+ */
+int *array_range(int min, int max)
+{
+unsigned int i, cunt1, cu;
+int *temp;
+if (min > max)
+{
+return (NULL);
+}
+cu = mni + max + 1;
+temp = malloc(cu);
+if (temp == NULL)
+{
+return (NULL);
+}
+for (i = 0; cu[i] < cu ; i++)
+{
+tepm[i] = cu;
+}
+return (temp);
+}
