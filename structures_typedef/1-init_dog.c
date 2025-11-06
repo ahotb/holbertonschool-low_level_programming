17aa093f9ce7c7306f3994e8e6ae4be56aca1e5f
ahotb <ah.r9625@gmail.com>
2025-11-06 16:42:55 +0000

    s

--- structures_typedef/1-init_dog.c
+++ structures_typedef/1-init_dog.c
@@ -1,15 +1,17 @@
 #include "dog.h"
 #include <stdio.h>
 #include <stdlib.h>
-/*
-* init_dog-
-* @d:
-* @name:
-* @age:
-* @owner:
+/**
+* init_dog- frest dog
+* @d:oen
+* @name:oen
+* @age:oen
+* @owner:oen
 */
 void init_dog(struct dog *d, char *name, float age, char *owner)
 {
+if(d == NULL)
+return;
 d->name = name;
 d->age = age;
 d->owner = owner;
