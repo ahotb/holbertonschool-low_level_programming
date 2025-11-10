9de35b506bc06a39030023b2c8a2ce7575d175ea
ahotb <ah.r9625@gmail.com>
2025-11-10 15:37:35 +0000

    2

---
+++ function_pointers/2-int_index.c
@@ -0,0 +1,31 @@
+#include <unistd.h>
+#include "function_pointers.h"
+
+/**
+* int_index - check the code
+* @array : check the code
+* @size : check the code
+* @cmp : check the code
+*
+*
+* Return: Always 0.
+*/
+
+int int_index(int *array, int size, int (*cmp)(int))
+{
+int i, r;
+
+if (size <=  0)
+{
+return (-1);
+}
+for (i = 0; i < size; i++)
+{
+r = cmp(array[i]);
+if (r != 0)
+{
+return (i);
+}
+}
+return (0);
+}
