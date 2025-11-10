ed4885ef53e4b29d0cd8ef6d50c098ef69e5f747
ahotb <ah.r9625@gmail.com>
2025-11-10 11:15:29 +0000

    w

---
+++ function_pointers/0-print_name.c
@@ -0,0 +1,14 @@
+#include <unistd.h>
+#include "function_pointers.h"
+
+/**
+* print_name - check the code
+* @name :
+* @f :
+* @ * :
+*
+*
+* Return: Always 0.
+*/
+
+void print_name(char *name, void (*f)(char *))
