a9fbfd139d54690de305dadf89ddc75d5f447212
ahotb <ah.r9625@gmail.com>
2025-11-13 15:05:07 +0000

    q

--- variadic_functions/3-print_all.c
+++ variadic_functions/3-print_all.c
@@ -8,59 +8,45 @@
  */
 
 
+
 void print_all(const char * const format, ...)
 {
-   va_list ab;
-   
+va_list ab;
 const char *p = format;
-int first = 1;
-int c; 
-int i; 
-double f; 
+char *pe = "";
+int c;
+int i;
+double f;
 char *s_val;
-
-  va_start(ab, format);
+va_start(ab, format);
 while (*p != '\0')
 {
-            
-         if (!first)
-            printf(", ");
-    switch (*p) {
-            case 'c': {
-                 c = va_arg(ab, int); 
-                
-                printf("%c", (char)c);
-                first = 0;
-                break;
-            }
-            case 'i': {
-                 i = va_arg(ab, int);
-                
-                printf("%d", i);
-                first = 0;
-                break;
-            }
-            case 'f': {
-                f = va_arg(ab, double);
-                       
-                printf("%f", f);
-                first = 0;
-                break;
-            }
-            case 's': {
-                s_val = va_arg(ab, char *);
-                        
-                if (s_val == NULL) {
-                    printf("(nil)");
-                    first = 0;
-                } else {
-                    printf("%s", s_val);
-                }
-                break;
-            }
-            default:
-                break;
-        }
+switch (*p) {
+case 'c':
+c = va_arg(ab, int);
+printf("%s%c", pe, (char)c);
+pe = ", ";
+break;
+case 'i':
+i = va_arg(ab, int);
+printf("%s%d", pe, i);
+pe = ", ";
+break;
+case 'f':
+f = va_arg(ab, double);
+printf("%s%f", pe, f);
+pe = ", ";
+break;
+case 's':
+s_val = va_arg(ab, char *);
+if (!s_val)
+s_val = "(nil)";
+printf("%s%s", pe, s_val);
+pe = ", ";
+break;
+default:
+break;
+}
 
 p++;
 }
