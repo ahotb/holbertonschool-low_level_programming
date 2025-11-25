efd6d29ebc973a8878f8ecdf2bbaa49f09427a9d
ahotb <ah.r9625@gmail.com>
2025-11-25 15:27:49 +0300

    Adding free_list

---
+++ singly_linked_lists/4-free_list.c
@@ -0,0 +1,20 @@
+#include <stdlib.h>
+#include <string.h>
+#include <stdio.h>
+#include "lists.h"
+
+/**
+ * free_list - frees a singly linked list
+ * @head: pointer to the head of the list
+ */
+void free_list(list_t *head)
+{
+    list_t *temp;
+    while (head != NULL)
+    {
+        temp = head->next;
+        free(head->str);
+        free(head);
+        head = temp;
+    }
+}
