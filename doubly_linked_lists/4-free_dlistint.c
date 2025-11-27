4159fa77a6fbb09e59f01e1ad85a0d40f3ba38f3
ahotb <ah.r9625@gmail.com>
2025-11-27 13:08:13 +0300

    fixed

--- doubly_linked_lists/4-free_dlistint.c
+++ doubly_linked_lists/4-free_dlistint.c
@@ -9,12 +9,11 @@
  */
 void free_dlistint(dlistint_t *head)
 {
-    dlistint_t *temp;
-        while (head != NULL)
-            {
-            temp = head->next;
-            free(head->next);
-            free(head);
-            head = temp;
-            }
+dlistint_t *temp;
+while (head != NULL)
+{
+temp = head->next;
+free(head);
+head = temp;
+}
 }
