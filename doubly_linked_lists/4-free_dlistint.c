509ace097cc4fa338cd5dadba1456951f8e45b54
ahotb <ah.r9625@gmail.com>
2025-11-27 13:03:27 +0300

    a

--- doubly_linked_lists/4-free_dlistint.c
+++ doubly_linked_lists/4-free_dlistint.c
@@ -8,13 +8,13 @@
  * @head: pointer to the head of the list
  */
 void free_dlistint(dlistint_t *head)
-    {
+{
     dlistint_t *temp;
         while (head != NULL)
             {
-            temp = head->next;
-            free(head->prev);
+            temp = head->prev;
+            free(head->next);
             free(head);
             head = temp;
             }
-    }
+}
