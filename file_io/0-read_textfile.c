714b841df92897e622d51f65ad8e4e1a54688356
ahotb <ah.r9625@gmail.com>
2025-11-30 17:45:11 +0300

    adding read_textfile

---
+++ file_io/0-read_textfile.c
@@ -0,0 +1,63 @@
+#include <stdio.h>
+#include <unistd.h>
+#include <fcntl.h>
+#include <stdlib.h>
+#include "main.h"
+/**
+ * read_textfile - reads a text file and prints it to the POSIX standard output
+ * @filename: pointer to the name of the file to be read
+ * @letters: number of letters it should read and print
+ * Return: actual number of letters it could read and print
+ *         0 if the file can not be opened or read,
+ *         0 if filename is NULL,
+ *         0 if write fails or does not write the expected amount of bytes
+ */
+ssize_t read_textfile(const char *filename, size_t letters)
+{
+	if (filename == NULL)
+	{
+		return (0);
+	}
+	ssize_t n_written;
+	int fd = open(filename, O_RDONLY);
+	if (fd == -1)
+	{
+		return (0);
+	}
+	char *buffer = malloc(letters);
+	if (buffer == NULL)
+	{
+		close(fd);
+		return (0);
+	}
+	ssize_t n_read = read(fd, buffer, letters);
+	if (n_read < 0)
+	{
+		free(buffer);
+		close(fd);
+		return (0);
+	}
+	if (n_read > 0)
+	{
+		n_written = write(STDOUT_FILENO, buffer, n_read);
+	}
+	if (n_read == 0)
+	{
+		free(buffer);
+		close(fd);
+		return (0);
+	}
+	if (n_written != n_read)
+	{
+		free(buffer);
+		close(fd);
+		return (0);
+	}
+	else
+	{
+		free(buffer);
+		close(fd);
+		return (n_read);
+	}
+	return (n_read);
+}
