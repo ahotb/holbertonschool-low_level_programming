c6eb04f616d367866def46d5a3eb11957a0a466f
ahotb <ah.r9625@gmail.com>
2025-11-30 17:51:57 +0300

    fixed

--- file_io/0-read_textfile.c
+++ file_io/0-read_textfile.c
@@ -14,23 +14,26 @@
  */
 ssize_t read_textfile(const char *filename, size_t letters)
 {
+	int fd;
+	char *buffer;
+	ssize_t n_read;
 	if (filename == NULL)
 	{
 		return (0);
 	}
 	ssize_t n_written;
-	int fd = open(filename, O_RDONLY);
+	fd = open(filename, O_RDONLY);
 	if (fd == -1)
 	{
 		return (0);
 	}
-	char *buffer = malloc(letters);
+	buffer = malloc(letters);
 	if (buffer == NULL)
 	{
 		close(fd);
 		return (0);
 	}
-	ssize_t n_read = read(fd, buffer, letters);
+	n_read = read(fd, buffer, letters);
 	if (n_read < 0)
 	{
 		free(buffer);
