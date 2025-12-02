ea6791febd2a848fe49cffcc5772ef12408e2fab
ahotb <ah.r9625@gmail.com>
2025-12-02 13:41:03 +0300

    fix

--- file_io/3-cp.c
+++ file_io/3-cp.c
@@ -27,10 +27,8 @@ int copy_file(int fd_from, int fd_to)
 	{
 		return (-2);
 	}
-	
 	return (0);
 }
-
 int main(int argc, char *argv[])
 {
 	int fd_form, fd_to;
@@ -38,45 +36,38 @@ int main(int argc, char *argv[])
 
 	if (argc != 3)
 	{
-		dprintf(STDERR_FILENO, "Usage: cp file_from file_to\n");
-		exit(97);
+		dprintf(STDERR_FILENO, "Usage: cp file_from file_to\n"), exit(97);
 	}
 	fd_form = open(argv[1], O_RDONLY);
 	if (fd_form == -1)
 	{
-		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", argv[1]);
-		exit(98);
+		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", argv[1]), exit(98);
 	}
 	fd_to = open(argv[2], O_WRONLY | O_CREAT | O_TRUNC, 0664);
 	if (fd_to == -1)
 	{
-		dprintf(STDERR_FILENO, "Error: Can't write to %s\n", argv[2]);
-		exit(99);
+		dprintf(STDERR_FILENO, "Error: Can't write to %s\n", argv[2]), exit(99);
 	}
 	result = copy_file(fd_form, fd_to);
 	if (result == -1)
 	{
 		close(fd_form);
 		close(fd_to);
-		dprintf(STDERR_FILENO, "Error: Can't write to %s\n", argv[2]);
-		exit(99);
+		dprintf(STDERR_FILENO, "Error: Can't write to %s\n", argv[2]), exit(99);
 	}
 	if (result == -2)
 	{
 		close(fd_form);
 		close(fd_to);
-		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", argv[1]);
-		exit(98);
+		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", argv[1]), exit(98);
 	}
 	if (close(fd_to) == -1)
 	{
-		dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", fd_to);
-		exit(100);
+		dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", fd_to), exit(100);
 	}
 	if (close(fd_form) == -1)
 	{
-		dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", fd_form);
-		exit(100);
+		dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", fd_form), exit(100);
 	}
 	return (0);
 }
