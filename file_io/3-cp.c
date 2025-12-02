949d33864b122f30ccb4e4625aca43dd45b7c96d
ahotb <ah.r9625@gmail.com>
2025-12-02 13:44:42 +0300

    fix

--- file_io/3-cp.c
+++ file_io/3-cp.c
@@ -35,9 +35,7 @@ int main(int argc, char *argv[])
 	int result;
 
 	if (argc != 3)
-	{
 		dprintf(STDERR_FILENO, "Usage: cp file_from file_to\n"), exit(97);
-	}
 	fd_form = open(argv[1], O_RDONLY);
 	if (fd_form == -1)
 	{
@@ -46,9 +44,7 @@ int main(int argc, char *argv[])
 	}
 	fd_to = open(argv[2], O_WRONLY | O_CREAT | O_TRUNC, 0664);
 	if (fd_to == -1)
-	{
 		dprintf(STDERR_FILENO, "Error: Can't write to %s\n", argv[2]), exit(99);
-	}
 	result = copy_file(fd_form, fd_to);
 	if (result == -1)
 	{
@@ -64,12 +60,8 @@ int main(int argc, char *argv[])
 		exit(98);
 	}
 	if (close(fd_to) == -1)
-	{
 		dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", fd_to), exit(100);
-	}
 	if (close(fd_form) == -1)
-	{
 		dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", fd_form), exit(100);
-	}
 	return (0);
 }
