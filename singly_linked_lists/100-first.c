c3f38f991b801ac8649bcb172044e93553ee9887
ahotb <ah.r9625@gmail.com>
2025-12-02 14:07:37 +0300

    adding The Hare and the Tortoise

---
+++ singly_linked_lists/100-first.c
@@ -0,0 +1,14 @@
+#include "lists.h"
+#include <stdio.h>
+
+/**
+ * first - Prints the first 10 elements of a singly linked list.
+ * @head: Pointer to the head of the list.
+ *
+ * Return: The number of nodes printed.
+ */
+
+ void __attribute__((constructor)) print_before_main(void)
+ {
+	printf("You're beat! and yet, you must allow,\nI bore my house upon my back!\n");
+}
