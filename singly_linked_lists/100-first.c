51a2002d821e2e03f7e7ec50a3cd783bfb367665
ahotb <ah.r9625@gmail.com>
2025-12-02 14:10:01 +0300

    fix

--- singly_linked_lists/100-first.c
+++ singly_linked_lists/100-first.c
@@ -8,7 +8,8 @@
  * Return: The number of nodes printed.
  */
 
- void __attribute__((constructor)) print_before_main(void)
- {
-	printf("You're beat! and yet, you must allow,\nI bore my house upon my back!\n");
+void __attribute__((constructor)) print_before_main(void)
+{
+	printf("You're beat! and yet, you must allow,\n");
+	printf("I bore my house upon my back!\n");
 }
