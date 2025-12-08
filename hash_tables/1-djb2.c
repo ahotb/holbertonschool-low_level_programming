98ac0695b83fa423d43d648feed796b70f97d505
ahotb <ah.r9625@gmail.com>
2025-12-08 17:32:15 +0300

    betty fix

--- hash_tables/1-djb2.c
+++ hash_tables/1-djb2.c
@@ -2,13 +2,13 @@
 #include "hash_tables.h"
 unsigned long int hash_djb2(const unsigned char *str)
 {
-    unsigned long int hash;
-    int c;
+	unsigned long int hash;
+	int c;
 
-    hash = 5381;
-    while ((c = *str++))
-    {
-        hash = ((hash << 5) + hash) + c; /* hash * 33 + c */
-    }
-    return (hash);
+	hash = 5381;
+	while ((c = *str++))
+	{
+		hash = ((hash << 5) + hash) + c; /* hash * 33 + c */
+	}
+	return (hash);
 }
