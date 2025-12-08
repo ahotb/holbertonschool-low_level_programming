555730ed97dc781f94828ffea40dc3a91d51e423
ahotb <ah.r9625@gmail.com>
2025-12-08 16:51:16 +0300

    adding creates a hash table

---
+++ hash_tables/0-hash_table_create.c
@@ -0,0 +1,23 @@
+#include "hash_tables.h"
+#include <stdio.h>
+
+hash_table_t *hash_table_create(unsigned long int size)
+{
+	hash_table_t *newtable;
+	int i;
+	newtable = malloc(sizeof(hash_table_t));
+	if (newtable == NULL)
+		return (NULL);
+	newtable->size = size;
+
+	newtable->array = malloc(size * sizeof(*(newtable->array)));
+	if (newtable->array == NULL)
+		free(newtable);
+		return (NULL);
+
+	for (i = 0; i < size ;i++)
+	{
+	newtable->array[i] = NULL;
+	}
+	return (newtable);
+}
