b74328087fc04ad067c4f1a168acf8cc478af5ef
ahotb <ah.r9625@gmail.com>
2025-12-08 17:02:25 +0300

    fix betty

--- hash_tables/0-hash_table_create.c
+++ hash_tables/0-hash_table_create.c
@@ -11,6 +11,7 @@ hash_table_t *hash_table_create(unsigned long int size)
 {
 	hash_table_t *newtable;
 	unsigned int i;
+
 	newtable = malloc(sizeof(hash_table_t));
 	if (newtable == NULL)
 	return(NULL);
