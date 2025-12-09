b29669fa42d14c83a1dbd119807c51b0b7516000
ahotb <ah.r9625@gmail.com>
2025-12-09 16:37:27 +0300

    fix betty

--- hash_tables/4-hash_table_get.c
+++ hash_tables/4-hash_table_get.c
@@ -20,7 +20,7 @@ char *hash_table_get(const hash_table_t *ht, const char *key)
 	}
 	index = key_index((const unsigned char *)key, ht->size);
 	current = ht->array[index];
-	
+
 	while (current != NULL)
 	{
 		if (strcmp(current->key, key) == 0)
