0a8b5bb11a2fbbe9cee3842fba5840e54921a740
ahotb <ah.r9625@gmail.com>
2025-12-26 20:03:41 +0300

    fix betty

--- variables_if_else_while/100-print_comb3.c
+++ variables_if_else_while/100-print_comb3.c
@@ -1,6 +1,6 @@
 #include <stdio.h>
 
-/* 
+/*
  * main - prints all possible different combinations of two digits
  *
  * Return: Always 0 (Success)
@@ -26,5 +26,5 @@ int main(void)
 		}
 	}
 	putchar('\n');
-	return 0;
+	return (0);
 }
