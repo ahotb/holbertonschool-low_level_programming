d056e1b54e8db1c941aff3a67b611da85b47975c
ahotb <ah.r9625@gmail.com>
2025-12-26 20:39:12 +0300

    fixed argstostr

--- malloc_free/100-argstostr.c
+++ malloc_free/100-argstostr.c
@@ -10,34 +10,30 @@
  */
 char *argstostr(int ac, char **av)
 {
-int t, i, len, j, total = 0;
-char *p;
-char *c;
-if (ac == 0 || av == NULL)
-{
-return (NULL);
-}
-for (t = 0; t < ac; t++)
-{
-for (len = 0; av[t][len] != '\0'; len++)
-{
-total += len + 1;
-}
-}
-p = malloc(total + 1);
-if (p == NULL)
-return (NULL);
-c = p;
-for (i = 0; i < ac; i++)
-{
-for (j = 0; av[i][j] != '\0'; j++)
-{
-*c = av[i][j];
-c++;
-}
-*c = '\n';
-c++;
-}
-*c = '\0';
-return (p);
+	int i, j, len, total = 0;
+	char *str, *ptr;
+	if (ac == 0 || av == NULL)
+		return (NULL);
+	for (i = 0; i < ac; i++)
+	{
+		for (j = 0; av[i][j]; j++)
+			;
+		total += j + 1;
+	}
+	str = malloc(sizeof(char) * total + 1);
+	if (str == NULL)
+		return (NULL);
+	ptr = str;
+	for (i = 0; i < ac; i++)
+	{
+		for (j = 0; av[i][j]; j++)
+		{
+			*ptr = av[i][j];
+			ptr++;
+		}
+		*ptr = '\n';
+		ptr++;
+	}
+	*ptr = '\0';
+	return (str);
 }
